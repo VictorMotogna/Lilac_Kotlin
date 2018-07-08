3259430074bc3e5a8e1ba92194568a826397525b
Victor Motogna <vmotogna@gmail.com>
2018-07-08 13:28:53 +0300

    demo setup

--- app/src/main/java/com/victormotogna/flowers/MainActivity.kt
+++ app/src/main/java/com/victormotogna/flowers/MainActivity.kt
@@ -2,48 +2,15 @@ package com.victormotogna.flowers
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.LinearLayoutManager
-import com.victormotogna.flowers.adapters.OrderAdapter
-import com.victormotogna.flowers.api.ApiClient
-import com.victormotogna.flowers.api.FlowersApi
-import com.victormotogna.flowers.model.Order
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.schedulers.Schedulers
-import kotlinx.android.synthetic.main.activity_main.*
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var jsonApi: FlowersApi
-    private var compositeDisposable: CompositeDisposable = CompositeDisposable()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // init api
-        val retrofit = ApiClient.instance
-        jsonApi = retrofit.create(FlowersApi::class.java)
-
-        // recycler view
-        rv_all_orders.setHasFixedSize(true)
-        rv_all_orders.layoutManager = LinearLayoutManager(this)
-        fetchData()
+        Toast.makeText(this, "Blank app. Too bad :(", Toast.LENGTH_SHORT).show()
     }
 
-    private fun fetchData() {
-        compositeDisposable.add(jsonApi.flowers
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe { orders -> displayData(orders) })
-    }
-
-    private fun displayData(orders: List<Order>?) {
-        val adapter = OrderAdapter(this, orders!!)
-        rv_all_orders.adapter = adapter
-
-        refresh_orders.setOnRefreshListener { fetchData() }
-        adapter.notifyDataSetChanged()
-        refresh_orders.isRefreshing = false
-    }
 }
