598cfcc5471ed920e714c1f6a86c383dd3653ec0
Matko Gabi <ytgabi98@gmail.com>
2018-07-16 23:36:19 +0300

    converted a function to lambda form

--- app/src/main/java/com/victormotogna/flowers/MainActivity.kt
+++ app/src/main/java/com/victormotogna/flowers/MainActivity.kt
@@ -11,33 +11,31 @@ import kotlinx.android.synthetic.main.activity_main.*
 import org.koin.android.ext.android.inject
 
 class MainActivity : AppCompatActivity() {
-    private val viewModel by inject<MainViewModel>()
 
-    private val adapter by lazy {
-        OrderAdapter(this, listOf())
-    }
+    private val viewModel by inject<MainViewModel>()
+    private val adapter by lazy { OrderAdapter(this, listOf()) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        rv_all_orders.setHasFixedSize(true)
-        rv_all_orders.layoutManager = LinearLayoutManager(this)
-        rv_all_orders.adapter = adapter
-
-        refresh_orders.setOnRefreshListener { viewModel.fetchData() }
-
+        setupUi()
 //        refresh_orders.isRefreshing = true
 //        viewModel.fetchData()
         observeModel()
     }
 
+    private fun setupUi() {
+        rv_all_orders.setHasFixedSize(true)
+        rv_all_orders.layoutManager = LinearLayoutManager(this)
+        rv_all_orders.adapter = adapter
+
+        refresh_orders.setOnRefreshListener { viewModel.fetchData() }
+    }
+
     private fun observeModel() {
-        viewModel.orderList.observe(this, object : Observer<List<Order>> {
-            override fun onChanged(t: List<Order>?) {
-                adapter.updateList(t ?: listOf())
-                refresh_orders.isRefreshing = false
-            }
+        viewModel.orderList.observe(this, Observer<List<Order>> { listOfFlowers ->
+            adapter.updateList(listOfFlowers ?: listOf())
+            refresh_orders.isRefreshing = false
         })
     }
 }
